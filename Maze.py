18ff5882caf9d6892e7ec5e31b7068b0df2c1484
Alvaro <90021982@study.curtincollege.edu.au>
2021-07-27 23:57:54 -0700

    renew viewing code

--- Maze.py
+++ Maze.py
@@ -1,4 +1,4 @@
-
+import GridViewer
 LL = 1
 START = 2
 END = 3
@@ -21,29 +21,7 @@ if __name__ == "__main__":
         [ WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL,  WALL, WALL],
     ]
                     
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
-            
-        print()
-
+    GridViewer.view(grid)
 
     print("Find a solution to get from ^^ to $$, using the characters " 
         + "'" + NORTH + "', '" + EAST + "', '" + SOUTH + "' and '" + WEST + "'"
@@ -108,25 +86,4 @@ if __name__ == "__main__":
         print("MESSAGE 5") # Did not reach the end.
 
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            
-            if grid[i][j] == EMPTY:
-                print("  ", end = "")
-                    
-            elif grid[i][j] == WALL:
-                print("##", end = "")
-                    
-            elif grid[i][j] == START:
-                print("^^", end = "")
-                    
-            elif grid[i][j] == END:
-                print("$$", end = "")
-                    
-            elif grid[i][j] == VISITED:
-                print("..", end = "")
-                    
-            else:
-                raise AssertionError
-            
-        print()
+    GridViewer.view(grid)
