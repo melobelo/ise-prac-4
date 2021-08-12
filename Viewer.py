253ba4d28706d417d9fb2c8d008bd8eaeded51c3
a <xoxo@gmail.com>
2021-08-12 10:02:33 -0700

    updated viewer.py

--- Viewer.py
+++ Viewer.py
@@ -16,7 +16,7 @@ def view(grid):
                 		print("$$", end = "")
                     
             		elif grid[i][j] == Maze.VISITED:
-                		print("..", end = "")
+                		print(",,", end = "")
                     
             		else:
                 		raise AssertionError
